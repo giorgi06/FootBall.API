539f94d2f5642c69a93b1e46ab0b0a1b9e986d92
PC <PC@PC>
2022-12-09 14:00:55 +0400

    Add project files.

---
+++ FootBall.API/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootBall.API.Controllers
+{
+    using FootBall.API.Context;
+    using FootBall.API.Models;
+
+    using Microsoft.EntityFrameworkCore;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext db;
+
+        public UserController(UserContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<IEnumerable<CreateUser>>> GetAll()
+
+        {
+            return await this.db.User.ToListAsync();
+        }
+
+        [HttpGet("[action]/{Id}")]
+        public async Task<ActionResult<CreateUser>> GetById(int id)
+        {
+            CreateUser user = await this.db.User.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+                return NotFound();
+
+            return new ObjectResult(user);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<CreateUser>> Create([FromQuery]CreateUser user)
+        {
+            if (user == null)
+                return this.BadRequest();
+
+            db.User.Add(user);
+
+            await this.db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult<CreateUser>> Update ([FromQuery]CreateUser user)
+        {
+            if (user == null)
+                return this.BadRequest();
+
+            if (!db.User.Any(x => x.Id == user.Id))
+                return this.NotFound();
+
+            db.User.Update(user);
+            await db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpDelete("[action]/{id}")]
+        public async Task<ActionResult<CreateUser>> Delete(int id)
+        {
+            CreateUser user = await this.db.User.FirstOrDefaultAsync(x => x.Id == id);
+             
+            if (id < 0)
+                return NotFound();
+
+            db.User.Remove(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+    }
+}
---
+++ FootBall.API/entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FootBall.API.entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "FirstName is Required field!")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "LastName is Required field!")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Age is Required field!")]
+        public int Age { get; set; }
+    }
+}
